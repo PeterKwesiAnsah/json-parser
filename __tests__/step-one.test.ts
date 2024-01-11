824f173efe97900e9c78b40aa015392300abc196
PeterKwesiAnsah <ansahpeter123@yahoo.com>
2024-01-11 14:30:43 +0000

    chore: step one test

---
+++ __tests__/step-one.test.ts
@@ -0,0 +1,21 @@
+const { parser } = require('../index');
+
+describe('JSON parser', () => {
+	let originalExit = () => {};
+	beforeEach(() => {
+		// Mock process.exit to prevent actual exit
+		originalExit = process.exit;
+		//@ts-expect-error
+		process.exit = jest.fn();
+	});
+
+	afterEach(() => {
+		// Restore original process.exit after each test
+		//@ts-expect-error
+		process.exit = originalExit;
+	});
+	it('Should exit for invalid JSON string', () => {
+		parser('5');
+		expect(process.exit).toHaveBeenCalledWith(1);
+	});
+});
